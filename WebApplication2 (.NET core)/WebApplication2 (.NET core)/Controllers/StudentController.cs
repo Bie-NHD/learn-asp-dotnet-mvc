0469400ba69d77721c0a8b7e7ac6205d23839532
Ng Huong Duyen <huongduyen.work@gmail.com>
2023-09-07 10:22:26 +0700

    first commit

---
+++ WebApplication2 (.NET core)/WebApplication2 (.NET core)/Controllers/StudentController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2__.NET_core_.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Delete() { return View(); }
+    }
+}
---
+++ WebApplication2 (.NET core)/WebApplication2 (.NET core)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication2__.NET_core_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
