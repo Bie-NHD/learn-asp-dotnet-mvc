090b88863d6dd16f1ee5260dd8efa0f22bc08108
Ng Huong Duyen <huongduyen.work@gmail.com>
2023-09-21 10:58:35 +0700

    learn views & lab 2

---
+++ Lab2_WebApplication/Lab2_WebApplication/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab2_WebApplication.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string ClassName { get; set; }
+    }
+}
